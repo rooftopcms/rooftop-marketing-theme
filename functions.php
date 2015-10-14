4a88513da1318d410e2895aeacd0666e30061c67
Ed Jones <ed@errorstudio.co.uk>
2015-10-14 11:53:34 +0100

    Add content types and stuff

--- functions.php
+++ functions.php
@@ -7,16 +7,19 @@ function setup_features() {
      *
      * https://codex.wordpress.org/Function_Reference/register_post_type
      */
-    RooftopCMS::addContentType('Feature');
+    RooftopCMS::addContentType('SDK Language');
+    RooftopCMS::addContentType('Product Feature');
+    RooftopCMS::addContentType('Product Plan');
+
 
     /**
-     * add a new taxonomy. The send argument can be either:
+    * add a new taxonomy. The send argument can be either:
      *
      * string: the content type you're adding this taxonomy to
      * array: an array of content types
      * string 'all' - to add the taxonomy to of your publicly accessible content types
      */
-    RooftopCMS::addTaxonomy('Feature Detail', 'all');
+    RooftopCMS::addTaxonomy('Product Benefits', 'Product Plan');
 }
 
 add_action('init', 'setup_features', 20);
